b808b17d54f8ae7b7d2b614355b1c412f5f6f180
Curtis Wan <wanchaoyi@automq.com>
2024-01-29 11:57:01 +0800

    test secrets
    Signed-off-by: Curtis Wan <wanchaoyi@automq.com>

--- goodbye.sh
+++ goodbye.sh
@@ -1,8 +1,8 @@
 echo "Goodbye"
-echo "${WEB_HOOK_URL}"
-if [ -z "${WEB_HOOK_URL}" ]; then
+echo "${HOOK_URL}"
+if [ -z "${HOOK_URL}" ]; then
   echo "WEB_HOOK_URL is empty"
   exit 1
 else
-  echo "length of WEB_HOOK_URL is ${#WEB_HOOK_URL}"
+  echo "length of WEB_HOOK_URL is ${#HOOK_URL}"
 fi
